41252585f526675146f4d788338a20162827e5ba
Piyush Mehta <me@piyushmehta.com>
2018-07-23 23:38:39 +0530

    Request
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- schema.js
+++ schema.js
@@ -6,7 +6,52 @@ const{
   GraphQLList,
   GraphQLNonNull
 }= require('graphql');
+//Hardcoded data
+const customers=[{
+  id:'1',
+  name:'Piyush Mehta',
+  email:'me@piyushmehta.com',
+  age:20
+},
+{
+  id: '2',
+  name: 'Hello Mehta',
+  email: 'me@hellomehta.com',
+  age: 21
+}, {
+  id: '3',
+  name: 'John Mehta',
+  email: 'me@Johnmehta.com',
+  age: 22
+}];
+const CustomerType = new GraphQLObjectType({
+  name:'Customer',
+  fields:()=>({
+    id: {type:GraphQLString},
+    name: {type:GraphQLString},
+    email: {type:GraphQLString},
+    age:{GraphQLInt}
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name:'RootQueryType',
+  customer:{
+    type:CustomerType,
+    args:{
+      id:{GraphQLString}
+    },
+    resolve(parentValue,args){
+      for(let i=0;i<customers.length;i++)
+      {
+        if(customers[i].id==args.id){
+          return customers[i];
+        }
+      }
+    }
+  }
+});
 
 module.exports=new GraphQLSchema({
-  
+
 });
