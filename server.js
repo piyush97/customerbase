161b5216b14d6e91e98c24188035db4edc10e38a
Piyush Mehta <me@piyushmehta.com>
2018-07-24 01:04:26 +0530

    Server modded
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express=require('express');
-const app=express();
-const expressGraphQL=require('express-graphql');
-const schema=require('./schema.js');
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
 
-app.use('/graphql',expressGraphQL({
-  schema:schema,
-  graphiql:true
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
 }));
-app.listen(4000,()=>{
-  console.log("Server is running on port 4000");
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000..');
 });
